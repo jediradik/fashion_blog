2ce7ebf1f6b7f34e95aca9c19d24618e321db612
Radyslav Dmytrenko <1991barbossa@gmail.com>
2021-01-21 22:35:26 +0200

    start to develop mobile version. Burger menu is complete

--- js/script.js
+++ js/script.js
@@ -3,4 +3,9 @@ $(function () {
     arrows: false,
     dots: true
   });
+
+  $('.burger-menu').on('click', function () {
+    $('.burger-menu').toggleClass('burger-menu__active');
+    $('.header-top__menu').toggleClass('header-top__menu-active');
+  });
 });
